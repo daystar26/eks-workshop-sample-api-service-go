eafef546e482fafa0bec8ae63b7d9232e671c5f0
Darryl Ramgoolam <Darryl.Ramgoolam@gmail.com>
2022-08-17 17:40:17 -0400

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Brooklyn"}
+		res := &response{Message: "Hello Brooklyn 2"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
